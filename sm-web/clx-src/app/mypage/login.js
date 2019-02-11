243e60ecdf059525d901d37e88e94329f35f77d1
saejung <39181415+saejung@users.noreply.github.com>
2019-02-11 16:11:43 +0900

    지원 포털 test

---
+++ sm-web/clx-src/app/mypage/login.js
@@ -0,0 +1,20 @@
+/************************************************
+ * login.js
+ * Created at 2019. 1. 28. 오후 3:41:39.
+ *
+ * @author bumil
+ ************************************************/
+
+
+
+/*
+ * "login" 버튼에서 click 이벤트 발생 시 호출.
+ * 사용자가 컨트롤을 클릭할 때 발생하는 이벤트.
+ */
+function onButtonClick(/* cpr.events.CMouseEvent */ e){
+	/** 
+	 * @type cpr.controls.Button
+	 */
+	var button = e.control;
+	alert("login");
+}
